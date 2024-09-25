cd7b932c25ffa7907baba2d82a8e0be0b42eb1ab
Natalia-tonn <natali-tonn@web.de>
2024-09-25 23:24:58 +0200

    add confirmation dialog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { AppBar, Box }from '@mui/material';
+import{ Toolbar} from '@mui/material';
+import {Typography, Button, Container} from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import {DialogActions} from '@mui/material';
+import {DialogContent }from '@mui/material';
+import {DialogContentText} from '@mui/material';
+import {DialogTitle }from '@mui/material';
+import { useState } from 'react';
+
+
+
 
 function App() {
+const [open, setOpen] = useState(false)
+
+
+const handleOpen = () => {
+  setOpen(true)
+}
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <AppBar  position="static">
+      <Toolbar>
+        <Typography variant='h5'> My Material UI App</Typography>
+      </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="sm" >
+        <Typography variant='h5' gutterBottom  > Добро пожаловать в наше приложение!</Typography>
+        <Button variant="contained"
+      color='primary'
+      onClick={handleOpen}
+        >Открыть диалоговое окно
+        </Button>
+
+<Box>
+  <Dialog open= {open} onClose={handleClose}>
+    <DialogTitle> Использовать Material UI?</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+  Вы можете настроить это приложение по своему усмотрению 
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleClose} >Закрыть </Button>
+      <Button onClick={handleClose} > Применить</Button> 
+    </DialogActions>
+  </Dialog>
+</Box>
+
+
+
+      </Container>
+
     </div>
   );
 }
